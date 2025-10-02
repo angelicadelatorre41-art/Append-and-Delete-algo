f0834099a943e3576296f7399118a29c797025aa
angelicadelatorre41-art <angelicadelatorre41@gmail.com>
2025-10-02 11:17:36 +0800

    Append and Delete.cs

---
+++ Append and Delete.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'appendAndDelete' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. STRING t
+     *  3. INTEGER k
+     */
+
+    public static string appendAndDelete(string s, string t, int k)
+    {
+        int commonLength = 0;
+        while (commonLength < s.Length && commonLength < t.Length && s[commonLength] == t[commonLength])
+        {
+            commonLength++;
+        }
+
+        int totalOps = (s.Length - commonLength) + (t.Length - commonLength);
+        
+        if (k >= totalOps && (k - totalOps) % 2 == 0 || k >= s.Length + t.Length)
+        {
+            return "Yes";
+        }
+
+        return "No";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string t = Console.ReadLine();
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.appendAndDelete(s, t, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
